69730eb86d96e071a321fd6da73749721ea55210
davidbetteridge <david.betteridge@zeromission.io>
2025-04-10 09:10:15 +0100

    final

--- csharp.xUnit/TestAll/Program.cs
+++ csharp.xUnit/TestAll/Program.cs
@@ -2,7 +2,7 @@
 
 using GildedRoseKata;
 
-using var file = File.CreateText("/Users/davidbetteridge/Personal/GildedRose-Refactoring-Kata_2025/csharp.xUnit/TestAll/changed.txt");
+using var file = File.CreateText("/Users/davidbetteridge/Personal/GildedRose-Refactoring-Kata_2025/csharp.xUnit/GildedRoseTests/AllTests.cs");
 
 for (var quality = 0; quality < 100; quality++)
 {
@@ -21,10 +21,24 @@ for (var quality = 0; quality < 100; quality++)
 
         foreach (var item in items)
         {
-            file.WriteLine($"Name: {item.Name}   Sellin: {item.SellIn}   Quality: {item.Quality}");
+            CreateTest(file, item.Name, sellin, quality, item.SellIn, item.Quality);
         }
+        
     }
 }
 
+void CreateTest(StreamWriter file, string itemName, int sellin, int qualtity, int finalSellin, int finalQuality)
+{ 
+    file.WriteLine(" [Fact]");
+    file.WriteLine($" public void {itemName.Replace(" ", "_").Replace(",","")}_{sellin}_{qualtity}()");
+    file.WriteLine(" {");
+    file.WriteLine("     var items = new List<Item> { new() { Name = \"" + itemName + "\", SellIn = " + sellin + ", Quality = "+qualtity+" } };");
+    file.WriteLine("     var app = new GildedRose(items);");
+    file.WriteLine("     app.UpdateQuality();");
+    file.WriteLine($"     Assert.Equal({finalSellin}, items[0].SellIn);");
+    file.WriteLine($"     Assert.Equal({finalQuality}, items[0].Quality);");
+    file.WriteLine(" }  ");
+    file.WriteLine();
+}
 
 
